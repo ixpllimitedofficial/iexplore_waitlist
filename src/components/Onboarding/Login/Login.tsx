36dfedbb9f33f80eeafb033451b1ebb2cc7b8971
Oluwaseyi <seyifagbemi211@gmail.com>
2023-11-22 14:41:14 +0100

    feat:made some changes to login page

--- src/components/Onboarding/Login/Login.tsx
+++ src/components/Onboarding/Login/Login.tsx
@@ -1,6 +1,10 @@
-import LoginForm from "./LoginForm";
+import Image from "next/image";
 import Link from "next/link";
+import LoginForm from "./LoginForm";
 import AppButton from "@/components/UI/Button/AppButton";
+
+import GoogleIcon from "@/assets/img/GoogleIcon.png";
+import AppleIcon from "@/assets/img/AppleIcon.png";
 
 const Login = () => {
   return (
@@ -17,16 +21,25 @@ const Login = () => {
       {/* divider */}
       <div className="flex items-center">
         <div className="border-t-2 border-[##F9D9AD] flex-grow"></div>
-        <div className="px-5 text-[#F9D9AD] text-xl font-bold">
-          or
-        </div>
+        <div className="px-5 text-[#F9D9AD] text-xl font-bold">or</div>
         <div className="border-t-2 border-[##F9D9AD] flex-grow"></div>
       </div>
 
       {/* continue with buttons */}
-      <button>Continue with Google</button>
-      <button>Continue with Apple</button>
+      <div className="flex items-center bg-white text-black p-3 gap-2 rounded-2xl justify-center">
+        <Image src={GoogleIcon} alt="GoogleIcon" />
+        <button> Continue with Google</button>
+      </div>
 
+      <div className="flex items-center bg-white text-black p-3 gap-2 rounded-2xl justify-center">
+        <Image src={AppleIcon} alt="AppleIcon" />
+        <button>Continue with Apple</button>
+      </div>
+
+      <p className="text-[#F7D098] text-center">
+        Yet to create an account?{" "}
+        <span className="underline font-bold"> Sign up</span>
+      </p>
     </section>
   );
 };
