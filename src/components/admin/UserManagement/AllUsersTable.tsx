8540eca34c5ba5c32802a3052e33b62af0c14949
Oluwaseyi <seyifagbemi211@gmail.com>
2024-03-01 11:36:58 +0100

    fix:changed text

--- src/components/admin/UserManagement/AllUsersTable.tsx
+++ src/components/admin/UserManagement/AllUsersTable.tsx
@@ -22,7 +22,7 @@ const AllUsersTable = () => {
   return (
     <section className="mt-5 bg-[#333333] p-5 rounded-2xl">
       <div className="flex justify-between">
-        <p className="font-bold text-2xl text-[#F7D098]">User Management</p>
+        <p className="font-bold text-2xl text-[#F7D098]">All users</p>
 
         <div className="flex items-center gap-5">
           <AppSearchInput
