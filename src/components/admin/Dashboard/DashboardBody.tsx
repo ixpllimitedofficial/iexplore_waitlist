74ebfcda9804e0712df662c7736f3904cdbb6828
Oluwaseyi <seyifagbemi211@gmail.com>
2024-03-01 09:43:57 +0100

    feat:dashboard page started

---
+++ src/components/admin/Dashboard/DashboardBody.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import React from "react";
+import { Calendar } from "@/components/UI/calendar";
+
+const DashboardBody = () => {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
+
+  return (
+    <>
+      {/* body 1 */}
+      <div className="grid grid-cols-10 gap-4 mt-5">
+        <div className="grid grid-cols-12 gap-4 col-span-6">
+          <div className="grid grid-cols-12 gap-4 col-span-full">
+            <div className="col-span-4 bg-[#333333] p-5 rounded-2xl">
+              TOTAL USERS
+            </div>
+            <div className="col-span-4 bg-[#333333] p-5 rounded-2xl">
+              TOTAL VENDORS
+            </div>
+            <div className="col-span-4 bg-[#333333] p-5 rounded-2xl">
+              ACTIVE USERS
+            </div>
+            <div className="col-span-4 bg-[#333333] p-5 rounded-2xl">
+              TOTAL REFERRAL
+            </div>
+            <div className="col-span-4 bg-[#333333] p-5 rounded-2xl">
+              NEW USERS
+            </div>
+            <div className="col-span-4 bg-[#333333] p-5 rounded-2xl">
+              TOTAL REVENUE
+            </div>
+          </div>
+
+          <div className="grid grid-cols-12 gap-4 col-span-full">
+            <div className="col-span-6 bg-[#333333] p-5 rounded-2xl">
+              Vendors traffic
+            </div>
+            <div className="col-span-6 bg-[#333333] p-5 rounded-2xl">
+              Influencers traffic
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-4 bg-[#333333] py-5 rounded-2xl flex justify-center">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            className="rounded-md border"
+          />
+        </div>
+      </div>
+
+      {/* body 2 */}
+      <div className="grid grid-cols-5 gap-4 mt-5">
+        <div className="col-span-3 bg-[#333333] p-5 rounded-2xl">
+          Users traffic
+        </div>
+        <div className="col-span-2 bg-[#333333] p-5 rounded-2xl">
+          Notifications
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DashboardBody;
---
+++ src/components/admin/Dashboard/DashboardHeader.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Image from "next/image";
+import DashboardNotificationsIcon from "@/assets/svg/AdminIconsSvg/DashboardNotificationsIcon.svg";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/UI/avatar";
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between">
+      <div>
+        <p className="font-bold text-3xl">Dashboard</p>
+        <p className="text-[#F7D098] mt-1">
+          Hi, Anita. Welcome back to iExplore Admin!
+        </p>
+      </div>
+
+      <div className="bg-[#F7D0984D] p-4 rounded-2xl flex items-center gap-4">
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+
+        <div>
+          <p className="text-[#F7D098] font-bold">Anita Cruz</p>
+          <p className="text-[#F7D098]">anita@commerce.com</p>
+        </div>
+
+        <Image
+          src={DashboardNotificationsIcon}
+          alt="DashboardNotificationsIcon"
+        />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
