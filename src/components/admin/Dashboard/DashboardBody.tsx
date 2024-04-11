af9627c19abe75403c7ede3374f43a402bca3c6c
Oluwaseyi <seyifagbemi211@gmail.com>
2024-04-11 12:29:41 +0100

    feat:dashboard mobile view started

--- src/components/admin/Dashboard/DashboardBody.tsx
+++ src/components/admin/Dashboard/DashboardBody.tsx
@@ -10,39 +10,39 @@ const DashboardBody = () => {
     <>
       {/* body 1 */}
       <div className="grid grid-cols-10 gap-4 mt-5">
-        <div className="grid grid-cols-12 gap-4 col-span-6">
+        <div className="grid grid-cols-12 gap-4 col-span-full lg:col-span-6">
           <div className="grid grid-cols-12 gap-4 col-span-full">
-            <div className="col-span-4 bg-[#333333] p-5 rounded-2xl">
+            <div className="col-span-6 lg:col-span-4 bg-[#333333] p-5 rounded-2xl">
               TOTAL USERS
             </div>
-            <div className="col-span-4 bg-[#333333] p-5 rounded-2xl">
+            <div className="col-span-6 lg:col-span-4 bg-[#333333] p-5 rounded-2xl">
               TOTAL VENDORS
             </div>
-            <div className="col-span-4 bg-[#333333] p-5 rounded-2xl">
+            <div className="col-span-6 lg:col-span-4 bg-[#333333] p-5 rounded-2xl">
               ACTIVE USERS
             </div>
-            <div className="col-span-4 bg-[#333333] p-5 rounded-2xl">
+            <div className="col-span-6 lg:col-span-4 bg-[#333333] p-5 rounded-2xl">
               TOTAL REFERRAL
             </div>
-            <div className="col-span-4 bg-[#333333] p-5 rounded-2xl">
+            <div className="col-span-6 lg:col-span-4 bg-[#333333] p-5 rounded-2xl">
               NEW USERS
             </div>
-            <div className="col-span-4 bg-[#333333] p-5 rounded-2xl">
+            <div className="col-span-6 lg:col-span-4 bg-[#333333] p-5 rounded-2xl">
               TOTAL REVENUE
             </div>
           </div>
 
           <div className="grid grid-cols-12 gap-4 col-span-full">
-            <div className="col-span-6 bg-[#333333] p-5 rounded-2xl">
+            <div className="col-span-full lg:col-span-6 bg-[#333333] p-5 rounded-2xl">
               Vendors traffic
             </div>
-            <div className="col-span-6 bg-[#333333] p-5 rounded-2xl">
+            <div className="col-span-full lg:col-span-6 bg-[#333333] p-5 rounded-2xl">
               Influencers traffic
             </div>
           </div>
         </div>
 
-        <div className="col-span-4 bg-[#333333] py-5 rounded-2xl flex justify-center">
+        <div className="hidden col-span-full lg:col-span-4 bg-[#333333] py-5 rounded-2xl lg:flex justify-center">
           <Calendar
             mode="single"
             selected={date}
@@ -54,13 +54,13 @@ const DashboardBody = () => {
 
       {/* body 2 */}
       <div className="grid grid-cols-5 gap-4 mt-5">
-        <div className="col-span-3 bg-[#333333] rounded-2xl">
+        <div className="col-span-full lg:col-span-3 bg-[#333333] rounded-2xl">
           <p className="font-bold m-5 text-2xl"> Users traffic</p>
 
           {/* divider */}
           <div className="border-t-2 border-[#979797] w-full mt-5 mb-5"></div>
         </div>
-        <div className="col-span-2 bg-[#333333] p-5 rounded-2xl">
+        <div className="col-span-full lg:col-span-2 bg-[#333333] p-5 rounded-2xl">
           Notifications
         </div>
       </div>
--- src/components/admin/Dashboard/DashboardHeader.tsx
+++ src/components/admin/Dashboard/DashboardHeader.tsx
@@ -5,23 +5,23 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/UI/avatar";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between">
-      <div>
-        <p className="font-bold text-3xl">Dashboard</p>
-        <p className="text-[#F7D098] mt-1">
+    <header className="flex flex-col-reverse gap-7 lg:flex-row items-center justify-between">
+      <div className="self-start">
+        <p className="font-bold text-xl lg:text-3xl">Dashboard</p>
+        <p className="text-[#F7D098] text-sm mt-1">
           Hi, Anita. Welcome back to iExplore Admin!
         </p>
       </div>
 
-      <div className="bg-[#F7D0984D] p-4 rounded-2xl flex items-center gap-4">
+      <div className="self-end bg-[#F7D0984D] p-2 lg:p-4 rounded-2xl flex items-center gap-2 lg:gap-4">
         <Avatar>
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
 
         <div>
-          <p className="text-[#F7D098] font-bold">Anita Cruz</p>
-          <p className="text-[#F7D098]">anita@commerce.com</p>
+          <p className="text-[#F7D098] text-sm lg:text-base font-bold">Anita Cruz</p>
+          <p className="text-[#F7D098] text-sm lg:text-base">anita@commerce.com</p>
         </div>
 
         <Image
