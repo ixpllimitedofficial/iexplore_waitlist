9e775d27fef95f5fcf93c2a663a638af13a78de6
Oluwaseyi <seyifagbemi211@gmail.com>
2024-04-18 10:56:11 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -8,6 +9,52 @@ const poppins = Poppins({
   variable: "--font-poppins",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
+
+// const myFont = localFont({
+//   src: [
+//     {
+//       path: "../../public/fonts/NeueMontreal-Light.otf",
+//       weight: "300",
+//       style: "normal",
+//     },
+//     {
+//       path: "../../public/fonts/NeueMontreal-LightItalic.otf",
+//       weight: "300",
+//       style: "italic",
+//     },
+//     {
+//       path: "../../public/fonts/NeueMontreal-Regular.otf",
+//       weight: "450",
+//       style: "normal",
+//     },
+//     {
+//       path: "../../public/fonts/NeueMontreal-Italic.otf",
+//       weight: "450",
+//       style: "italic",
+//     },
+//     {
+//       path: "../../public/fonts/NeueMontreal-Medium.otf",
+//       weight: "530",
+//       style: "normal",
+//     },
+//     {
+//       path: "../../public/fonts/NeueMontreal-MediumItalic.otf",
+//       weight: "530",
+//       style: "italic",
+//     },
+//     {
+//       path: "../../public/fonts/NeueMontreal-Bold.otf",
+//       weight: "800",
+//       style: "normal",
+//     },
+//     {
+//       path: "../../public/fonts/NeueMontreal-BoldItalic.otf",
+//       weight: "800",
+//       style: "italic",
+//     },
+//   ],
+//   variable: "--font-neue",
+// });
 
 export const metadata: Metadata = {
   title: "iExplore",
@@ -25,7 +72,6 @@ export default function RootLayout({
         <link rel="icon" href="/icon.svg" />
       </head>
       <body>
-        {/* <Navbar /> */}
         {children}
       </body>
     </html>
