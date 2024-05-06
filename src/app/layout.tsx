ad70f8f7d7f757056075764c5bef03384763b58b
Oluwaseyi <seyifagbemi211@gmail.com>
2024-05-06 23:16:13 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ const inter = Inter({
   subsets: ["latin"],
   display: "swap",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-inter"
 });
 
 export const metadata: Metadata = {
