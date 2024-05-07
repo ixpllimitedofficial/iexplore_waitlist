5afaab81dea8492a313474ce9db0219bedca021b
Oluwaseyi <seyifagbemi211@gmail.com>
2024-05-07 13:23:55 +0100

    update

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,34 +1,27 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
-export const userStore = create(
-  persist(
-    (set) => ({
-      user: {},
-      loginUser: async (data: any) => {
-        try {
-          const response = await fetch("/api/user", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-          });
-          const newData = await response.json();
+export const userStore = create((set) => ({
+  user: {},
+  loginUser: async (data: any) => {
+    try {
+      const response = await fetch("/api/user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const newData = await response.json();
 
-          if (!response.ok) {
-            throw new Error("Failed to fetch external data");
-          }
+      if (!response.ok) {
+        throw new Error("Failed to fetch external data");
+      }
 
-          set({ user: newData });
-          console.log(newData);
-        } catch (error) {
-          console.error("Error fetching external data:", error);
-        }
-      },
-    }),
-    {
-      name: "user-data",
+      set({ user: newData });
+      console.log(newData);
+    } catch (error) {
+      console.error("Error fetching external data:", error);
     }
-  )
-);
+  },
+}));
