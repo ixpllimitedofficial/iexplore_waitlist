c8ea1f92b02cf893098bd384c8f73e9150ee8193
Oluwaseyi <seyifagbemi211@gmail.com>
2024-06-25 21:59:58 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,34 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-module.exports = nextConfig
+// module.exports = nextConfig;
+
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://44.193.73.68:8000/api/:path*', // Proxy to Backend
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Forwarded-Proto',
+            value: 'https',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
