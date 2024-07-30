dd11b046de557d34d3fe4e92bd216bad9e92c4a2
Oluwaseyi <seyifagbemi211@gmail.com>
2024-07-30 18:44:44 +0100

    feat: waiting list desktop page completed

--- src/components/MainPage/Waitlist/SpotsOnPhoneSection.tsx
+++ src/components/MainPage/Waitlist/SpotsOnPhoneSection.tsx
@@ -4,13 +4,10 @@ import * as React from "react";
 import Image from "next/image";
 import Autoplay from "embla-carousel-autoplay";
 
-import { Card, CardContent } from "@/components/UI/card";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
 } from "@/components/UI/carousel";
 
 import PhoneCard from "@/assets/img/MainWebsite/PhoneCard.png";
@@ -26,39 +23,24 @@ const SpotsOnPhoneSection = () => {
       opts={{
         loop: true,
       }}
-        // onMouseEnter={plugin.current.stop}
-      //   onMouseLeave={plugin.current.reset}
     >
       <Image
         src={Phone}
         alt="Phone"
-        // className="absolute top-[70%] left-1/2 w-[370px] transform -translate-x-1/2 -translate-y-1/2"
-        className="absolute top-[70%] left-1/2 w-[90%] max-w-[370px] transform -translate-x-1/2 -translate-y-1/2"
+        className="absolute top-[70%] left-1/2 w-[80vw] max-w-[370px] transform -translate-x-1/2 -translate-y-1/2"
       />
-      {/* <CarouselContent className="-ml-1">
-        {Array.from({ length: 7 }).map((_, index) => (
-          <CarouselItem key={index} className="pl-6 md:basis-1/2 lg:basis-1/4">
-            <Image
-              src={PhoneCard}
-              alt="PhoneCard"
-              className="w-[300px] h-auto"
-            />
-          </CarouselItem>
-        ))}
-      </CarouselContent> */}
+
       <CarouselContent className="relative -ml-1">
         {Array.from({ length: 7 }).map((_, index) => (
-          <CarouselItem key={index} className="pl-4 md:basis-1/2 lg:basis-1/4">
+          <CarouselItem key={index} className="pl-4 md:basis-1/3 xl:basis-1/4">
             <Image
               src={PhoneCard}
               alt="PhoneCard"
-              className="w-[80%] max-w-[300px] h-auto mx-auto"
+              className="w-[63vw] max-w-[300px] h-auto mx-auto"
             />
           </CarouselItem>
         ))}
       </CarouselContent>
-      {/* <CarouselPrevious />
-      <CarouselNext /> */}
     </Carousel>
   );
 };
