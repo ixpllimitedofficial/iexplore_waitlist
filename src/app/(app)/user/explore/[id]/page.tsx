36171544b47ec9d07b8e87327ff8a196ca95b128
Oluwaseyi <seyifagbemi211@gmail.com>
2024-09-25 05:09:03 +0100

    bug fix

--- src/app/(app)/user/explore/[id]/page.tsx
+++ src/app/(app)/user/explore/[id]/page.tsx
@@ -1,27 +1,20 @@
-"use client";
-
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import ArrowLeft from "@/assets/svg/ArrowLeft.svg";
+import Link from "next/link";
 
 const page = () => {
-  const router = useRouter();
-
   return (
     <section className="px-5">
       <div className="flex items-center">
-        <Image
-          src={ArrowLeft}
-          alt="ArrowLeft"
-          className="justify-start"
-          onClick={() => {
-            router.back();
-          }}
-        />
+        <Link href="/user/explore">
+          <Image src={ArrowLeft} alt="ArrowLeft" className="justify-start" />
+        </Link>
 
         <div className="flex justify-center items-center gap-3 w-full">
           <p className=" font-bold text-3xl">Maxxa beach bar</p>
-          <div className="bg-gold-500 py-1 px-3 rounded-3xl font-bold text-brandDark">4.5</div>
+          <div className="bg-gold-500 py-1 px-3 rounded-3xl font-bold text-brandDark">
+            4.5
+          </div>
         </div>
       </div>
     </section>
