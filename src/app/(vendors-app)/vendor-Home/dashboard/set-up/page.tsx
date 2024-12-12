a4b7a4f66f8beec4d269842f9e6857ae6db0f355
Ugochukwu4u <ugochukwupeter4u@gmail.com>
2024-12-12 16:19:35 +0100

    updated

--- src/app/(vendors-app)/vendor-Home/dashboard/set-up/page.tsx
+++ src/app/(vendors-app)/vendor-Home/dashboard/set-up/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 import Header from "@/components/vendor-components/MiniHeader/Header";
 import Link from "next/link";
 import Image from "next/image";
 import ArrowLeft from "@/assets/svg/ArrowLeft.svg";
 import UploadFileIcon from "@mui/icons-material/UploadFile";
+import { UploadCloud } from "lucide-react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
@@ -27,6 +29,7 @@ import {
   SelectValue,
 } from "@/components/UI/select";
 import { ToggleGroup, ToggleGroupItem } from "@/components/UI/toggle-group";
+import { Toggle } from "@/components/UI/toggle";
 import { Checkbox } from "@/components/UI/checkbox";
 import { Input } from "@/components/UI/input";
 import PasswordField from "@/components/UI/Inputs/PasswordField";
@@ -35,10 +38,19 @@ import { toast } from "@/components/UI/use-toast";
 import { inputStyling } from "@/utils/constant";
 import { setupBusinessValidationSchema } from "@/types/authSchemas";
 
+type UploadedFile = {
+  preview: string;
+} & File; // Extending the File type to include the preview property
+
 const Page = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [businessPhotos, setBusinessPhotos] = useState<UploadedFile[]>([]);
+  const [utilityBill, setUtilityBill] = useState<UploadedFile | null>(null);
+  const [registrationCertificate, setRegistrationCertificate] =
+    useState<UploadedFile | null>(null);
   const [imageUrl, setImageUrl] = useState<string | null>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [btnState, setBtnState] = useState(false);
 
   const form = useForm<z.infer<typeof setupBusinessValidationSchema>>({
     resolver: zodResolver(setupBusinessValidationSchema),
@@ -67,6 +79,54 @@ const Page = () => {
 
   const handleUploadClick = () => {
     fileInputRef.current?.click();
+  };
+  // Generalized onDrop handler
+  const createOnDrop = (setFileState: Function, multiple: boolean = false) =>
+    useCallback((acceptedFiles: File[]) => {
+      const previews = acceptedFiles.map((file) =>
+        Object.assign(file, { preview: URL.createObjectURL(file) })
+      );
+      if (multiple) {
+        setFileState((prev: UploadedFile[]) => [...prev, ...previews]);
+      } else {
+        setFileState(previews[0]);
+      }
+    }, []);
+
+  // Dropzones
+  const {
+    getRootProps: getBusinessRootProps,
+    getInputProps: getBusinessInputProps,
+  } = useDropzone({
+    onDrop: createOnDrop(setBusinessPhotos, true),
+    accept: { "image/*": [] },
+  });
+
+  const {
+    getRootProps: getUtilityRootProps,
+    getInputProps: getUtilityInputProps,
+  } = useDropzone({
+    onDrop: createOnDrop(setUtilityBill),
+    accept: { "image/*": [] },
+  });
+
+  const { getRootProps: getCertRootProps, getInputProps: getCertInputProps } =
+    useDropzone({
+      onDrop: createOnDrop(setRegistrationCertificate),
+      accept: { "image/*": [] },
+    });
+
+  // Handle reset for previews
+  const handleReset = (
+    setFileState: Function,
+    file: UploadedFile | UploadedFile[] | null
+  ) => {
+    if (Array.isArray(file)) {
+      file.forEach((f) => URL.revokeObjectURL(f.preview));
+    } else if (file) {
+      URL.revokeObjectURL(file.preview);
+    }
+    setFileState(Array.isArray(file) ? [] : null);
   };
   async function onSubmit(data: z.infer<typeof setupBusinessValidationSchema>) {
     // setBtnState(true);
@@ -247,24 +307,282 @@ const Page = () => {
                   )}
                 />
               </div>
-              <p className="text-2xl font-light">
+              <p className="text-2xl font-light mb-5">Sport Category</p>
+              <ToggleGroup
+                type="single"
+                className="gap-5 justify-start flex-wrap"
+              >
+                <ToggleGroupItem
+                  value="bars & pubs"
+                  aria-label="Toggle bars & pubs"
+                  className="bg-[#4D4D4D66] text-[#4D4D4D] w-[30%] text-2xl py-10 text-left border border-[#4D4D4D] hover:bg-gold-500 data-[state=on]:bg-gold-500 data-[state=on]:border-none"
+                >
+                  Bars & Pubs
+                </ToggleGroupItem>
+                <ToggleGroupItem
+                  value="clubs"
+                  aria-label="Toggle clubs"
+                  className="bg-[#4D4D4D66] text-[#4D4D4D] w-[30%] text-2xl py-10 text-left border border-[#4D4D4D] hover:bg-gold-500 data-[state=on]:bg-gold-500 data-[state=on]:border-none"
+                >
+                  Clubs
+                </ToggleGroupItem>
+                <ToggleGroupItem
+                  value="hotels"
+                  aria-label="Toggle hotels"
+                  className="bg-[#4D4D4D66] text-[#4D4D4D] w-[33%] text-2xl py-10 text-left border border-[#4D4D4D] hover:bg-gold-500 data-[state=on]:bg-gold-500 data-[state=on]:border-none"
+                >
+                  Hotels
+                </ToggleGroupItem>
+                <ToggleGroupItem
+                  value="resturants"
+                  aria-label="Toggle resturants"
+                  className="bg-[#4D4D4D66] text-[#4D4D4D] w-[30%] text-2xl py-10 text-left border border-[#4D4D4D] hover:bg-gold-500 data-[state=on]:bg-gold-500 data-[state=on]:border-none"
+                >
+                  Resturants
+                </ToggleGroupItem>
+                <ToggleGroupItem
+                  value="lounges"
+                  aria-label="Toggle lounges"
+                  className="bg-[#4D4D4D66] text-[#4D4D4D] w-[30%] text-2xl py-10 text-left border border-[#4D4D4D] hover:bg-gold-500 data-[state=on]:bg-gold-500 data-[state=on]:border-none"
+                >
+                  Lounges
+                </ToggleGroupItem>
+                <ToggleGroupItem
+                  value="beach parties"
+                  aria-label="Toggle beach parties"
+                  className="bg-[#4D4D4D66] text-[#4D4D4D] w-[33%] text-2xl py-10 text-left border border-[#4D4D4D] hover:bg-gold-500 data-[state=on]:bg-gold-500 data-[state=on]:border-none"
+                >
+                  Beach Parties
+                </ToggleGroupItem>
+              </ToggleGroup>
+
+              <p className="text-2xl font-bold text-center mb-5 mt-10">
                 Business Features and Listings
               </p>
               {/*closing hour*/}
-              <ToggleGroup type="multiple">
-                <ToggleGroupItem value="beer" aria-label="Toggle beer">
+              <ToggleGroup
+                type="multiple"
+                className="gap-5 w-[60%] flex-wrap justify-center items-center mx-auto"
+              >
+                <ToggleGroupItem
+                  value="beer"
+                  aria-label="Toggle beer"
+                  className="border broder-[#4D4D4D] text-[#4D4D4D] p-4 px-4 rounded-full hover:bg-gold-500 data-[state=on]:bg-gold-500 data-[state=on]:border-none"
+                >
                   <p>Beer</p>
                 </ToggleGroupItem>
                 <ToggleGroupItem
                   value="cocktails"
                   aria-label="Toggle cocktails"
+                  className="border broder-[#4D4D4D] text-[#4D4D4D] p-4 px-4 rounded-full hover:bg-gold-500 data-[state=on]:bg-gold-500 data-[state=on]:border-none"
                 >
                   <p>Cocktails</p>
                 </ToggleGroupItem>
-                <ToggleGroupItem value="security" aria-label="Toggle security">
+                <ToggleGroupItem
+                  value="security"
+                  aria-label="Toggle security"
+                  className="border broder-[#4D4D4D] text-[#4D4D4D] p-4 px-4 rounded-full hover:bg-gold-500 data-[state=on]:bg-gold-500 data-[state=on]:border-none"
+                >
                   <p>Security</p>
                 </ToggleGroupItem>
+                <ToggleGroupItem
+                  value="dinning"
+                  aria-label="Toggle dinning"
+                  className="border broder-[#4D4D4D] text-[#4D4D4D] p-4 px-4 rounded-full hover:bg-gold-500 data-[state=on]:bg-gold-500 data-[state=on]:border-none"
+                >
+                  <p>Dinning</p>
+                </ToggleGroupItem>
+                <ToggleGroupItem
+                  value="takeaway"
+                  aria-label="Toggle takeaway"
+                  className="border broder-[#4D4D4D] text-[#4D4D4D] p-4 px-4 rounded-full hover:bg-gold-500 data-[state=on]:bg-gold-500 data-[state=on]:border-none"
+                >
+                  <p>Takeaway</p>
+                </ToggleGroupItem>
+                <ToggleGroupItem
+                  value="spirits"
+                  aria-label="Toggle spirits"
+                  className="border broder-[#4D4D4D] text-[#4D4D4D] p-4 px-4 rounded-full hover:bg-gold-500 data-[state=on]:bg-gold-500 data-[state=on]:border-none"
+                >
+                  <p>Spirits</p>
+                </ToggleGroupItem>
+                <ToggleGroupItem
+                  value="food"
+                  aria-label="Toggle food"
+                  className="border broder-[#4D4D4D] text-[#4D4D4D] p-4 px-4 rounded-full hover:bg-gold-500 data-[state=on]:bg-gold-500 data-[state=on]:border-none"
+                >
+                  <p>Food</p>
+                </ToggleGroupItem>
+                <ToggleGroupItem
+                  value="strippers"
+                  aria-label="Toggle strippers"
+                  className="border broder-[#4D4D4D] text-[#4D4D4D] p-4 px-4 rounded-full hover:bg-gold-500 data-[state=on]:bg-gold-500 data-[state=on]:border-none"
+                >
+                  <p>Strippers</p>
+                </ToggleGroupItem>
+                <ToggleGroupItem
+                  value="parking space"
+                  aria-label="Toggle parking space"
+                  className="border broder-[#4D4D4D] text-[#4D4D4D] p-4 px-4 rounded-full hover:bg-gold-500 data-[state=on]:bg-gold-500 data-[state=on]:border-none"
+                >
+                  <p>Parking space</p>
+                </ToggleGroupItem>
+                <ToggleGroupItem
+                  value="reservation"
+                  aria-label="Toggle reservation"
+                  className="border broder-[#4D4D4D] text-[#4D4D4D] p-4 px-4 rounded-full hover:bg-gold-500 data-[state=on]:bg-gold-500 data-[state=on]:border-none"
+                >
+                  <p>Reservation</p>
+                </ToggleGroupItem>
+                <ToggleGroupItem
+                  value="online payment"
+                  aria-label="Toggle online payment"
+                  className="border broder-[#4D4D4D] text-[#4D4D4D] p-4 px-4 rounded-full hover:bg-gold-500 data-[state=on]:bg-gold-500 data-[state=on]:border-none"
+                >
+                  <p>Online Payment</p>
+                </ToggleGroupItem>
               </ToggleGroup>
+              <div className="max-w-lg mx-auto p-6">
+                {/* Business Photos */}
+                <section className="mb-10">
+                  <p className="text-2xl font-bold text-center mb-5">
+                    Add photos of business
+                  </p>
+                  {businessPhotos.length === 0 ? (
+                    <div
+                      {...getBusinessRootProps()}
+                      className="border-2 border-dashed border-gold-500 bg-[#4D4D4D] rounded-lg p-6 text-center cursor-pointer"
+                    >
+                      <input {...getBusinessInputProps()} />
+                      <UploadCloud className="mx-auto text-gold-500 text-lg" />
+                      <p className="text-xl font-bold py-2">
+                        Choose files or drag & drop
+                      </p>
+                      <p className="font-light">
+                        JPEG, PNG formats, up to 50MB
+                      </p>
+                    </div>
+                  ) : (
+                    <div className="mt-4">
+                      <div className="flex gap-4 overflow-x-auto pb-4">
+                        {businessPhotos.map((file, index) => (
+                          <Image
+                            key={index}
+                            src={file.preview}
+                            alt={`Business Preview ${index}`}
+                            width={96}
+                            height={96}
+                            className="w-24 h-24 object-cover rounded-lg"
+                          />
+                        ))}
+                      </div>
+                      <button
+                        type="button"
+                        onClick={() =>
+                          handleReset(setBusinessPhotos, businessPhotos)
+                        }
+                        className="flex gap-2 p-2 text-white rounded-md hover:bg-gold-500 transition-colors"
+                      >
+                        <UploadCloud />
+                        Change Photos
+                      </button>
+                    </div>
+                  )}
+                </section>
+
+                {/* Utility Bill */}
+                <section className="mb-10">
+                  <p className="text-2xl font-bold text-center mb-5">
+                    Add utility bill for business validation
+                  </p>
+                  {utilityBill === null ? (
+                    <div
+                      {...getUtilityRootProps()}
+                      className="border-2 border-dashed border-gold-500 bg-[#4D4D4D] rounded-lg p-6 text-center cursor-pointer"
+                    >
+                      <input {...getUtilityInputProps()} />
+                      <UploadCloud className="mx-auto text-gold-500 text-lg" />
+                      <p className="text-xl font-bold py-2">
+                        Choose a file or drag & drop
+                      </p>
+                      <p className="font-light">
+                        JPEG, PNG formats, up to 50MB
+                      </p>
+                    </div>
+                  ) : (
+                    <div className="mt-4 text-center">
+                      <Image
+                        src={utilityBill.preview}
+                        alt="Utility Bill Preview"
+                        width={120}
+                        height={120}
+                        className="w-full h-44 object-cover rounded-lg"
+                      />
+                    </div>
+                  )}
+                </section>
+
+                {/* Registration Certificate */}
+                <section>
+                  <p className="text-2xl font-bold text-center mb-5">
+                    Add CAC for business verification
+                  </p>
+                  {registrationCertificate === null ? (
+                    <div
+                      {...getCertRootProps()}
+                      className="border-2 border-dashed border-gold-500 bg-[#4D4D4D] rounded-lg p-6 text-center cursor-pointer"
+                    >
+                      <input {...getCertInputProps()} />
+                      <UploadCloud className="mx-auto text-gold-500 text-lg" />
+                      <p className="text-xl font-bold py-2">
+                        Choose a file or drag & drop
+                      </p>
+                      <p className="font-light">
+                        JPEG, PNG formats, up to 50MB
+                      </p>
+                    </div>
+                  ) : (
+                    <div className="mt-4 text-center">
+                      <Image
+                        src={registrationCertificate.preview}
+                        alt="Certification of Registration Preview"
+                        width={120}
+                        height={120}
+                        className="w-full h-44 object-cover rounded-lg"
+                      />
+                    </div>
+                  )}
+                </section>
+              </div>
+              <div className="w-[50%] mx-auto">
+                {/*cac number*/}
+                <FormField
+                  control={form.control}
+                  name="cac_number_of_business"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-lg">
+                        Type Business CAC number:
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          className={`${inputStyling}`}
+                          type="text"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button
+                  className="bg-[#4D4D4D] w-full mt-10 hover:bg-white transition duration-200 text-[#B0B0B0] px-8 py-5 lg:py-6 rounded-3xl font-bold text-base flex-grow"
+                  type="submit"
+                  disabled={btnState}
+                >
+                  {!btnState ? "Save" : "saving..."}
+                </Button>
+              </div>
             </form>
           </Form>
         </div>
