660334b7514ed03b6e60afab8792b5c03530ca20
Ugochukwu4u <ugochukwupeter4u@gmail.com>
2024-12-19 18:16:51 +0100

    updated vendors Notification page

---
+++ src/app/(vendors-app)/vendor-Home/notifications/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "@/components/vendor-components/MiniHeader/Header";
+import NotificationTabs from "@/components/vendor-components/NotificationCard/NotificationTabs/page";
+
+const Page = () => {
+  return (
+    <section className="">
+      <Header title="Notifications" className="hidden md:block" />
+
+      {/*Notification cards */}
+      <div className="mt-20 md:mt-0 mx-5">
+        <NotificationTabs />
+      </div>
+    </section>
+  );
+};
+
+export default Page;
